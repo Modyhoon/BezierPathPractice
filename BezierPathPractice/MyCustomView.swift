7e5f8a048d2a8c6fc9d78bb0872f408b8f31a552
nonalias <ktw1532@naver.com>
2021-10-18 19:47:12 +0900

    Bezier Path 경로대로 움직이는 이미지 구현

--- BezierPathPractice/MyCustomView.swift
+++ BezierPathPractice/MyCustomView.swift
@@ -9,46 +9,61 @@ import UIKit
 
 class MyCustomView: UIView {
 
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    var pathLayer: CAShapeLayer = CAShapeLayer()
+    var button: UIButton = UIButton()
+    let imageView = UIImageView(image: UIImage(named: "star"))
+    var path = UIBezierPath()
 
-        let argument: [CGFloat] = [10, 20, 70]
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
 
-        var startAngle: CGFloat = -4 / .pi
-        if argument.reduce(0, { $0 + $1 }) == 100 {
-            argument.enumerated().forEach({
-                let endAngle: CGFloat = startAngle + CGFloat($0.element / 100 * 2) * .pi
-                let path = UIBezierPath()
-                path.addArc(withCenter: CGPoint(x: 150, y: 250), radius: 150, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-                startAngle = endAngle
-                path.addLine(to: CGPoint(x: 150, y: 250))
-                path.close()
-                switch $0.offset {
-                case 0:
-                    UIColor.red.set()
-                case 1:
-                    UIColor.blue.set()
-                case 2:
-                    UIColor.orange.set()
-                default: break
-                }
-                path.fill()
-                path.lineWidth = 10
-                UIColor.white.set()
-                path.stroke()
-            })
-        }
+    func configure() {
+        self.addSubview(self.button)
+        self.button.frame = CGRect(x: 100, y: 200, width: 50, height: 50)
+        self.button.addTarget(self, action: #selector(self.onButtonTapped(_:)), for: .touchUpInside)
+        self.button.backgroundColor = .orange
 
+        let starbucksCup = UIImageView(image: UIImage(named: "starbucksCup"))
+        starbucksCup.frame = CGRect(x: 150, y: 250, width: 100, height: 100)
+        self.addSubview(starbucksCup)
 
+        path.move(to: CGPoint(x: 50, y: 0))
+        path.addCurve(to: CGPoint(x: 200, y: 300), controlPoint1: CGPoint(x: 20, y: 150), controlPoint2: CGPoint(x: 20, y: 200))
 
-//        path.lineWidth = 5
-//        UIColor.black.set()
-//        path.stroke()
+        //Create a CAShape Layer
+        pathLayer.frame = CGRect(x: 50, y: 0, width: 50, height: 50)
+        pathLayer.path = path.cgPath
+        pathLayer.fillColor = nil
+        pathLayer.lineJoin = .bevel
+    }
 
-        let centerPath = UIBezierPath()
-        centerPath.addArc(withCenter: CGPoint(x: 150, y: 250), radius: 100, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
-        UIColor.white.set()
-        centerPath.fill()
+    @objc func onButtonTapped(_ sender: UIButton) {
+        self.addSubview(self.imageView)
+
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position"
+        animation.repeatCount = 1
+        animation.duration = 1.0
+
+        animation.path = path.cgPath
+        animation.delegate = self
+
+//        imageView.bounds = CGRect(x: 0, y: 0, width: 50, height: 50)
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50) // 이 차이는 뭘까?? 위치가 초기화 되고 안되고 차이...
+        imageView.layer.add(animation, forKey: "move image")
+    }
+}
+
+extension MyCustomView: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        self.pathLayer.removeFromSuperlayer()
+        print("animation end")
     }
 }
